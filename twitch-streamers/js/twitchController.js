1bbbf6da601f39a43f3078b4edf98c3f26ee700d
Xiumo Zhan <xiumozhan@gmail.com>
2017-02-15 15:44:30 -0800

    Add twitchService to get data from twitch api

--- twitch-streamers/js/twitchController.js
+++ twitch-streamers/js/twitchController.js
@@ -1,3 +1,12 @@
-twitchApp.controller('twitchController', ['$scope', '$http', function($scope, $http) {
+twitchApp.controller('twitchController', ['$scope', '$http', 'twitchService', function($scope, $http, twitchService) {
+    $scope.channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
+    $scope.channels.forEach(function(channel) {
+        twitchService.getStreams(channel).then(function(data) {
+            console.log(data);
+            twitchService.getChannels(channel).then(function(data) {
+                console.log(data);
+            });
+        });
+    });
 }]);
---
+++ twitch-streamers/js/twitchService.js
@@ -0,0 +1,17 @@
+twitchApp.service('twitchService', ['$http', '$sce', function($http, $sce) {
+    this.getChannels = function(channel) {
+        var request = {
+            method: 'jsonp',
+            url: $sce.trustAsResourceUrl('https://wind-bow.gomix.me/twitch-api/channels/' + channel)
+        };
+        return $http(request);
+    };
+
+    this.getStreams = function(stream) {
+        var request = {
+            method: 'jsonp',
+            url: $sce.trustAsResourceUrl('https://wind-bow.gomix.me/twitch-api/streams/' + stream)
+        };
+        return $http(request);
+    }
+}]);
