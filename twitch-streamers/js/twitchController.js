20f14a75ac2a964fca17dc8e92b71fc2d30f7064
Xiumo Zhan <xiumozhan@gmail.com>
2017-02-16 01:06:51 -0800

    Add filter to streamers depending on their status

--- twitch-streamers/js/twitchController.js
+++ twitch-streamers/js/twitchController.js
@@ -5,44 +5,64 @@ twitchApp.controller('twitchController', ['$scope', '$http', 'twitchService', fu
                         "dreamhackcs", "feardota", "merlinidota", "universedota", "arteezy",
                          "zai", "eternalenvyy", "test_channel", "comster40"];
     $scope.streams = [];
+    $scope.selected = 'all';
 
-    $scope.loadStreamData = function() {
+    $scope.statusMap = {
+        All: 'all',
+        Online: 'online',
+        Offline: 'offline'
+    };
+
+    $scope.filterChannels = function(status) {
+        $scope.streams = [];
+        $scope.loadStreamData($scope.selected);
+    };
+
+    $scope.loadStreamData = function(selection) {
         $scope.channels.forEach(function(channel) {
             twitchService.getStreams(channel).then(function(data) {
                 var streamingData = data.data.stream;
                 if(streamingData !== null) {
-                    $scope.streams.push({
-                        name: streamingData.channel.display_name,
-                        status: 'online',
-                        details: {
-                            game: streamingData.game,
-                            currentStreaming: streamingData.channel.status,
-                            viewer: streamingData.viewers,
-                            streamPreview: streamingData.preview,
-                            streamerLogo: streamingData.channel.logo,
-                            latestActivatedAt: streamingData.channel.updated_at,
-                            streamingUrl: streamingData.channel.url,
-                        }
-                    });
+                    if(selection === 'all' || selection === 'online') {
+                        $scope.streams.push({
+                            name: streamingData.channel.display_name,
+                            status: 'online',
+                            details: {
+                                game: streamingData.game,
+                                currentStreaming: streamingData.channel.status,
+                                viewer: streamingData.viewers,
+                                streamPreview: streamingData.preview,
+                                streamerLogo: streamingData.channel.logo,
+                                latestActivatedAt: streamingData.channel.updated_at,
+                                streamingUrl: streamingData.channel.url,
+                            }
+                        });
+                    }
+
                 } else {
                     twitchService.getUsers(channel).then(function(data) {
                         var userData = data.data;
                         var rootUrl = 'https://www.twitch.tv/';
                         if(userData.name !== undefined) {
-                            $scope.streams.push({
-                                name: userData.display_name,
-                                status: 'offline',
-                                details: {
-                                    streamingUrl: rootUrl + channel,
-                                    streamerLogo: userData.logo
-                                }
-                            });
+                            if(selection === 'all' || selection === 'offline') {
+                                $scope.streams.push({
+                                    name: userData.display_name,
+                                    status: 'offline',
+                                    details: {
+                                        streamingUrl: rootUrl + channel,
+                                        streamerLogo: userData.logo
+                                    }
+                                });
+                            }
+
                         } else {
-                            $scope.streams.push({
-                                name: channel,
-                                status: 'error',
-                                message: 'This user account does not exist or has been removed'
-                            });
+                            if(selection === 'all') {
+                                $scope.streams.push({
+                                    name: channel,
+                                    status: 'error',
+                                    message: 'This user account does not exist or has been removed'
+                                });
+                            }
                         }
 
                     });
@@ -51,7 +71,8 @@ twitchApp.controller('twitchController', ['$scope', '$http', 'twitchService', fu
 
             });
         });
-    }
+    };
 
-    $scope.loadStreamData();
+    $scope.loadStreamData($scope.selected);
+
 }]);
